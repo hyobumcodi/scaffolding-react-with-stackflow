cced28651b5671a4bfe66ac42d0b60e27fac998b
hyobumcodi <hb.lee@bodycodi.com>
2024-03-13 15:29:41 +0900

    feat: add vercel speed-insight

--- src/providers/Provider.tsx
+++ src/providers/Provider.tsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from 'stores';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 import { theme, global } from 'styles';
 import QueryProvider from './QueryProvider';
 
@@ -11,6 +12,7 @@ function AppProvider({ children }: PropsWithChildren) {
   return (
     <>
       <Analytics />
+      <SpeedInsights />
       <Global styles={global} />
       <ThemeProvider theme={theme}>
         <Provider store={store}>
